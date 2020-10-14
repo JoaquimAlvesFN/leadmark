516d4bb8b41765260171c222848cfc90a791fb11
Joaquim Alves <jafn92@gmail.com>
2020-10-14 11:54:23 -0300

    adicionando as rotas de navegacao entre as telas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,19 +3,28 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Login from '../pages/Login';
+import Home from '../pages/Home';
+import Product from '../pages/Product';
 
 const {Navigator, Screen} = createStackNavigator();
 
 function App() {
     return (
         <NavigationContainer>
-            <Navigator>
+            <Navigator screenOptions={{
+                headerShown: false
+            }}>
                 <Screen 
                     name="Login" 
                     component={Login} 
-                    options={{
-                        headerShown: false
-                    }}
+                />
+                <Screen 
+                    name="Home" 
+                    component={Home} 
+                />
+                <Screen 
+                    name="Product" 
+                    component={Product} 
                 />
             </Navigator>
         </NavigationContainer>
